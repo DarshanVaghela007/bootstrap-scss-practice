4abe65715c80db0e7bb897c170083fa2a4fd25fa
Darshan Vaghela <vagheladarshan085@gmail.com>
2020-05-25 17:24:46 +0530

    [complete] bootstrap-practice-25th-april

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'Bootstrap-Practice';
+export class AppComponent implements OnInit {
+  title = "Bootstrap-Practice";
+  alert: boolean = false;
+  ngOnInit() {
+    setTimeout(() => {
+      this.alert = true;
+    }, 2000);
+
+    setTimeout(() => {
+      this.alert = false;
+    }, 3000);
+  }
 }
